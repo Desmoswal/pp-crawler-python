aaf6255ba2ad4faf68f57af108c3c741b81388c5
desmoswal <mark.palmai94@gmail.com>
2019-10-27 19:18:30 +0100

    Updated parallel

--- Crawler.py
+++ Crawler.py
@@ -3,6 +3,8 @@ import urllib.parse
 import re
 import time
 import pika
+import multiprocessing
+
 
 url = "http://hu.kristof.wtf/maildir/"
 forbiddenNames = ["Name", "Size", "Last modified", "Description", "Parent Directory"]
@@ -11,42 +13,62 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)
 
-queue = []
-
-
 def list_dir(dir):
-    global queue
     req = urllib.request.Request(dir)
     path = req.full_url
     resp = urllib.request.urlopen(req)
     html = resp.read()
     strhtml = html.decode()
     results = re.findall("<a href=\".*\">(?P<name>.*)</a>", strhtml)
-    dirlist = [results for results in results if results not in forbiddenNames]
 
+    dirlist = []
+
+    for result in results:
+        if result not in forbiddenNames:
+            dirlist.append(path+result)
+
+    #dirlist = [results for results in results if results not in forbiddenNames]
+    #print(dirlist)
+
+    return dirlist
+
+def fill_queue(dirlist, q):
     while dirlist:
-        queueitem = path + dirlist.pop()
+        queueitem = dirlist.pop()
         #print("ADDED ITEM: " + queueitem)
-        queue.append(queueitem)
+        q.put(queueitem)
         #print("append")
+        #print(q.qsize())
 
-
-def scan_directory(urllist):
-    if urllist.endswith('.'):
-        print("FILE FOUND: " + urllist)
-        channel.basic_publish(exchange='', routing_key='task_queue', body=urllist)
-    elif urllist.endswith('/'):
-        print("DIRECTORY FOUND: " + urllist)
-        list_dir(urllist)
-
+def scan_directory(dirlist):
+    for dir in dirlist:
+        #print("DIR:" + dir)
+        if dir.endswith('.'):
+            #print("FILE FOUND: " + dir)
+            channel.basic_publish(exchange='', routing_key='task_queue', body=dir)
+        elif dir.endswith('/'):
+            #print("DIRECTORY FOUND: " + dir)
+            scan_directory(list_dir(dir))
 
 startTime = time.time()
-list_dir(url)
+#list_dir(url)
 
-while queue:
+if __name__ == '__main__':
+    manager = multiprocessing.Manager()
+    q = manager.Queue()
 
-    item = queue.pop()
-    #print("scanning" + item)
-    scan_directory(item)
+    fill_queue(list_dir(url), q)
+    while q.empty() is False:
+        #print("WHILE")
+        #print("uj process")
+        item = q.get()
+        p = multiprocessing.Process(target=scan_directory, args=(list_dir(item),))
+        #print("uj item: " + item)
+        #print("scanning" + item)
+        #scan_directory(item)
+        p.start()
+        p.join()
+        p.terminate()
+        #print(q.qsize())
 
-print("finished in : {}".format(time.time() - startTime))
+    print("finished in : {}".format(time.time() - startTime))
